af85321ac27f98c18bad86193c15b5058f57e6b3
nkvinay <nkvinay005.com>
2021-05-24 22:41:37 +0530

    two more lines added in the Helloworld class

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,6 +6,8 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
     System.out.println("Hello, Devops");
+    System.out.println("Hello, Java");
+    System.out.println("Hello, Jenkins");
 
   }
 }
